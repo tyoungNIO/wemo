faa7d17ef38a0dffeebda8d74351dc575d3de600
Tom Young <tyoung@n.io>
2018-09-24 16:32:49 -0600

    rediscover devices in case of error

--- tests/test_wemo_insight_block.py
+++ tests/test_wemo_insight_block.py
@@ -1,3 +1,5 @@
+from time import sleep
+from requests.exceptions import ConnectionError
 from unittest.mock import patch, MagicMock
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
@@ -28,3 +30,35 @@ class TestExample(NIOBlockTestCase):
         self.assertDictEqual(
             self.last_notified[DEFAULT_TERMINAL][1].to_dict(),
             {'pi': 3.14, 'foo': 'baz'})
+
+    @patch(WeMoInsight.__module__ + '.pywemo')
+    def test_rediscovery(self, mock_pywemo):
+        """ Retry discovery if it has failed."""
+        mock_insight = MagicMock()
+        mock_insight.insight_params = {'pi': 3.14}
+        mock_pywemo.discover_devices.side_effect = [
+            ConnectionError, [mock_insight]]
+        blk = WeMoInsight()
+        self.configure_block(blk, {})
+        blk.start()
+        self.assertTrue(blk._discovering)
+        self.assertEqual(mock_pywemo.discover_devices.call_count, 1)
+        # ConnectionError raised
+        sleep(0.1)
+        # discovery aborted
+        self.assertFalse(blk._discovering)
+
+        blk.process_signals([Signal()])
+        self.assertEqual(mock_pywemo.discover_devices.call_count, 2)
+        # device discovered
+        self.assertEqual(blk.device, mock_insight)
+        # signal was dropped, so no params retrieved from device
+        self.assertEqual(mock_insight.update_insight_params.call_count, 0)
+        # and nothing notified
+        self.assert_num_signals_notified(0)
+
+        blk.process_signals([Signal()])
+        self.assertEqual(mock_pywemo.discover_devices.call_count, 2)
+        self.assertEqual(mock_insight.update_insight_params.call_count, 1)
+        blk.stop()
+        self.assert_num_signals_notified(1)
--- wemo_insight_block.py
+++ wemo_insight_block.py
@@ -1,3 +1,4 @@
+from requests.exceptions import ConnectionError
 import pywemo
 from nio import Block, Signal
 from nio.block.mixins.enrich.enrich_signals import EnrichSignals
@@ -13,6 +14,7 @@ class WeMoInsight(Block, EnrichSignals):
         super().__init__()
         self.device = None
         self._thread = None
+        self._discovering = False
 
     def configure(self, context):
         super().configure(context)
@@ -25,14 +27,20 @@ class WeMoInsight(Block, EnrichSignals):
         if not self.device:
             self.logger.error('No WeMo device connected, dropping {} signals'\
                 .format(len(signals)))
-            return
+            if self._discovering:
+                return
+            else:
+                self._thread = spawn(self._discover)
+                return
         try:
-            self.logger.debug('Reading values from device...'.format(self.device))
+            self.logger.debug('Reading values from {}...'\
+                .format(self.device.name))
             self.device.update_insight_params()
         except AttributeError:
             # raised when pywemo has given up retrying
             self.logger.error('Unable to connect to WeMo, dropping {} signals'\
                 .format(len(signals)))
+            self.device = None
             return
         outgoing_signals = []
         for signal in signals:
@@ -42,11 +50,18 @@ class WeMoInsight(Block, EnrichSignals):
         self.notify_signals(outgoing_signals)
 
     def _discover(self):
+        self._discovering = True
         devices = []
         while not devices:
             self.logger.debug('Discovering WeMo devices on network...')
-            devices = pywemo.discover_devices()
+            try:
+                devices = pywemo.discover_devices()
+            except ConnectionError:
+                self.logger.error('Error discovering devices, aborting')
+                self._discovering = False
+                return
         self.logger.debug('Found {} WeMo devices'.format(len(devices)))
         self.device=devices[0]
         self.logger.debug('Selected device {} with MAC {}'.format(
             self.device.name, self.device.mac))
+        self._discovering = False
